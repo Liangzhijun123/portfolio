3e673da4d1dc180d337dfd51b4ebdcdf52766b6b
Ansh-Sonkusare <sonkusare.satish12@gmail.com>
2024-02-24 22:03:59 +0530

    fix: About

--- src/component/ui/about_me.tsx
+++ src/component/ui/about_me.tsx
@@ -2,23 +2,24 @@ function about_me() {
   return (
     <>
       <div className="mt-40 p-10">
-        <h1 className="font-tt-backwards text-[50px] text-[#735E59]">
-          About me
-        </h1>
-        <div className="bg-black h-0.5 w-full"></div>
+        <div className="w-full px-56">
+          <h1 className="font-tt-backwards text-6xl font-medium text-[#735E59]">
+            About me
+          </h1>
+          <div className="bg-black h-0.5 w-full"></div>
+        </div>
         <div className="flex flex-col items-center justify-center">
-          <h1 className="font-canva-sans text-[#735E59] font-bold text-[32px]">
+          <h1 className="font-canva-sans text-[#735E59] font-semibold text-5xl my-8">
             Hello There!
           </h1>
         </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-tt-commons-pro text-[17px]">
+        <div className="flex flex-col items-center justify-center gap-8 px-80 text-2xl font-extralight text-center ">
+          <p className="font-tt-commons-pro ">
             I'm a second-year student at Rochester Institute of Technology, with
             a passion for web and mobile computing.{" "}
           </p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-tt-commons-pro text-[17px]">
+
+          <p className="font-tt-commons-pro ">
             As a web and mobile computing major, I thrive on bringing ideas to
             life through design and collaboration. There's something incredibly
             fulfilling about working alongside a partner to create innovative
@@ -26,10 +27,8 @@ function about_me() {
             or diving deep into coding, the collaborative process has pushes me
             beyond the limits.{" "}
           </p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          {" "}
-          <p className="font-tt-commons-pro text-[17px]">
+
+          <p className="font-tt-commons-pro ">
             One of my greatest joys is designing websites that not only look
             stunning but also deliver seamless user experiences. I love
             experimenting with different effects and techniques to enhance the
