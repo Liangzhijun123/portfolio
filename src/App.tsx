7e13954f0f66a8f04c3f19b9d7206fba2efe0b0d
zhi_jun <liangzhijun1216@gmail.com>
2024-02-25 13:42:18 -0500

    feat: contact me

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from "./component/ui/nav";
 import Title from "./component/ui/title";
 import About from "./component/ui/about_me";
 import Do from "./component/ui/what_I_can_do";
+import Contact from "./component/ui/contact";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Title />
         <About />
         <Do />
+        <Contact />
       </div>
     </>
   );
---
+++ src/component/ui/contact.tsx
@@ -0,0 +1,50 @@
+function contact_me() {
+  return (
+    <>
+      <div className="mt-40 p-8">
+        <div className="w-full px-8">
+          <h1 className="font-tt-backwards text-6xl font-medium text-[#735E59]">
+            I'd love to work with you. Contact me now.
+          </h1>
+          <div className="bg-black h-0.5 w-full"></div>
+        </div>
+        <div className="grid grid-cols-2 px-60 mt-16">
+          <div>
+            <img src="/conn.png" width={400} height={200}></img>
+          </div>
+          <div className="flex flex-col text-left justify-center  ">
+            <div className="font-tt-commons-pro text-[24px] font-bold text-[#544541]">Phone</div>
+            <div className="font-tt-commons-pro text-[20px]">(917) 608-2248</div>
+            <div className="font-tt-commons-pro text-[24px] font-bold text-[#544541]">Email</div>
+            <div className="font-tt-commons-pro text-[20px]">liangzhijun1216@gmail.com</div>
+            <div className="font-tt-commons-pro text-[24px] font-bold text-[#544541]">Social</div>
+            <div className="flex flex-row">
+              <div>
+                <a href="https://www.facebook.com/profile.php?id=100009393358604" target="_blank">
+                  <img src="/fb.png"></img>
+                </a>
+              </div>
+              <div>
+                <a href="https://www.instagram.com/swankiersta/" target="_blank">
+                  <img src="/insta.png"></img>
+                </a>
+              </div>
+              <div>
+                <a href="https://twitter.com/n2nitrogenacid" target="_blank">
+                  <img src="/twitter.png"></img>
+                </a>
+              </div>
+              <div>
+                <a href="https://www.linkedin.com/in/zhi-jun-liang-9b1752228/" target="_blank">
+                  <img src="/linkein.png"></img>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default contact_me;
