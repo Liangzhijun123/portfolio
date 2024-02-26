5ce760c05592b1e045812b7f67c2d04070090f8f
Ansh-Sonkusare <sonkusare.satish12@gmail.com>
2024-02-27 02:42:05 +0530

    feat: Basic Animation

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,28 @@ import Title from "./component/ui/title";
 import About from "./component/ui/about_me";
 import Do from "./component/ui/what_I_can_do";
 import Contact from "./component/ui/contact";
-
+import Slide from "./component/ui/slide";
+import "./App.css";
 export default function App() {
   return (
     <>
       <div className="">
         <Navbar />
-        <Title />
-        <About />
-        <Do />
-        <Contact />
+
+        <Slide>
+          <Title />
+        </Slide>
+        <Slide>
+          <About />
+        </Slide>
+
+        <Slide>
+          <Do />
+        </Slide>
+
+        <Slide>
+          <Contact />
+        </Slide>
       </div>
     </>
   );
