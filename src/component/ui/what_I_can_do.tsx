6ac6a92f6309ae2c50d1cede430acc8af752459c
TeakMirror113 <59761933+Ansh-Sonkusare@users.noreply.github.com>
2024-02-28 03:06:11 +0530

    fix: product section grid (#4)
    * fix: Project section grid
    
    * refactor: unused import
    
    * refactor: unused import

--- src/component/ui/what_I_can_do.tsx
+++ src/component/ui/what_I_can_do.tsx
@@ -6,116 +6,104 @@ function can_do() {
           <h1 className="font-tt-backwards text-6xl font-medium text-[#735E59]">
             What I can do for you
           </h1>
-          <div className="bg-black h-0.5 w-full"></div>
+          <div className="bg-black h-0.5 "></div>
         </div>
 
-        <div className="grid grid-cols-3  relative mt-16 gap-20 overflow-hidden">
-          <div>
-            <img src="/pic.png" alt="myimage" height={200} width={500} />
-            <div className="bg-black h-0.5 w-full mt-5"></div>
-            <div className="grid grid-cols-2">
-              <div className="font-tt-commons-pro font-bold text-[#544541]">
-                Project
-              </div>
-              <div className="font-tt-commons-pro text-[#544541]">
-                E-commerce
-              </div>
-              <div className="font-tt-commons-pro font-bold text-[#544541]">
-                Year
-              </div>
-              <div className="font-tt-commons-pro text-[#544541]">2024</div>
+        <div className="grid 2xl:grid-cols-3 lg:grid-cols-2  relative mt-16  overflow-hidden">
+          <div className="flex flex-col items-center">
+            <div>
+              <img
+                className="pb-4 border-b-2 border-black mb-2"
+                src="/pic.png"
+                alt="myimage"
+                height={200}
+                width={500}
+              />
 
-              <div className="font-tt-commons-pro font-bold text-[#544541]">
-                Type
-              </div>
-              <div className="font-tt-commons-pro text-[#544541]">
-                Group project
-              </div>
-              <div>
-                {" "}
-                <a href="https://github.com/Liangzhijun123/ecom">
-                  <img src="/github.png"></img>
-                </a>
-              </div>
-              <div>
-                {" "}
-                <a href="https://www.figma.com/file/r31HvMoSMr2TOsxFFvhZCr/e-commerce-site-for-a-camera-store?type=design&node-id=0%3A1&mode=design&t=q9X17JKuU5KE5YIy-1">
-                  <img src="/figma.png"></img>
-                </a>
+              <div className="grid grid-cols-2">
+                <div className="font-tt-commons-pro font-bold text-[#544541]">
+                  Project
+                </div>
+                <div className="font-tt-commons-pro text-[#544541]">
+                  E-commerce
+                </div>
+                <div className="font-tt-commons-pro font-bold text-[#544541]">
+                  Year
+                </div>
+                <div className="font-tt-commons-pro text-[#544541]">2024</div>
+
+                <div className="font-tt-commons-pro font-bold text-[#544541]">
+                  Type
+                </div>
+                <div className="font-tt-commons-pro text-[#544541]">
+                  Group project
+                </div>
               </div>
             </div>
           </div>
 
-          <div>
-            <img src="/pic1.png" alt="myimage" height={200} width={500} />
-            <div className="bg-black h-0.5 w-full mt-5"></div>
-            <div className="grid grid-cols-2">
-              <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
-                Project
-              </div>
-              <div className="font-tt-commons-pro text-[#544541] text-[14px]">
-                Crochet Cuteness
-              </div>
-              <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
-                Year
-              </div>
-              <div className="font-tt-commons-pro text-[#544541] text-[14px]">
-                2024
-              </div>
-              <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
-                Type
-              </div>
-              <div className="font-tt-commons-pro text-[#544541] text-[14px]">
-                Commerical/business (work in progress)
-              </div>
-              <div>
-                {" "}
-                <a href="#">
-                  <img src="/github.png"></img>
-                </a>
-              </div>
-              <div>
-                {" "}
-                <a href="https://www.figma.com/file/7xJNdNuXfmmeVSLFXG7AFu/shop?type=design&mode=design&t=q9X17JKuU5KE5YIy-1">
-                  <img src="/figma.png"></img>
-                </a>
+          <div className="flex flex-col items-center">
+            <div>
+              <img
+                className="pb-4 border-b-2 border-black mb-2"
+                src="/pic1.png"
+                alt="myimage"
+                height={200}
+                width={500}
+              />
+
+              <div className="grid grid-cols-2">
+                <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
+                  Project
+                </div>
+                <div className="font-tt-commons-pro text-[#544541] text-[14px]">
+                  Crochet Cuteness
+                </div>
+                <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
+                  Year
+                </div>
+                <div className="font-tt-commons-pro text-[#544541] text-[14px]">
+                  2024
+                </div>
+                <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
+                  Type
+                </div>
+                <div className="font-tt-commons-pro text-[#544541] text-[14px]">
+                  Commerical/business (work in progress)
+                </div>
               </div>
             </div>
           </div>
 
-          <div>
-            <img src="/pic2.png" alt="myimage" height={200} width={500} />
-            <div className="bg-black h-0.5 w-full mt-5"></div>
-            <div className="grid grid-cols-2">
-              <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
-                Project
-              </div>
-              <div className="font-tt-commons-pro text-[#544541 text-[14px]]">
-                Travel to China
-              </div>
-              <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
-                Year
-              </div>
-              <div className="font-tt-commons-pro text-[#544541] text-[14px]">
-                2023
-              </div>
-              <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
-                Type
-              </div>
-              <div className="font-tt-commons-pro text-[#544541] text-[14px]">
-                Travel website
-              </div>
-              <div>
-                {" "}
-                <a href="#">
-                  <img src="/github.png"></img>
-                </a>
-              </div>
-              <div>
-                {" "}
-                <a href="https://www.figma.com/file/CqYgZKIQIfic8eJEJu2RiE/china-website?type=design&node-id=0%3A1&mode=design&t=Sfcr6CjK8mOxy2fJ-1">
-                  <img src="/figma.png"></img>
-                </a>
+          <div className="flex items-center flex-col">
+            <div>
+              <img
+                className="pb-4 border-b-2 border-black mb-2"
+                src="/pic2.png"
+                alt="myimage"
+                height={200}
+                width={500}
+              />
+
+              <div className="grid grid-cols-2">
+                <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
+                  Project
+                </div>
+                <div className="font-tt-commons-pro text-[#544541 text-[14px]]">
+                  Travel to China
+                </div>
+                <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
+                  Year
+                </div>
+                <div className="font-tt-commons-pro text-[#544541] text-[14px]">
+                  2023
+                </div>
+                <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
+                  Type
+                </div>
+                <div className="font-tt-commons-pro text-[#544541] text-[14px]">
+                  Travel website
+                </div>
               </div>
             </div>
           </div>
