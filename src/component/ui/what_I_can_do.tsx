64c548c32fa95576c909c3a3150dc282a9446099
zhi_jun <liangzhijun1216@gmail.com>
2024-04-03 18:56:06 -0400

    feat: added another personal web design project

--- src/component/ui/what_I_can_do.tsx
+++ src/component/ui/what_I_can_do.tsx
@@ -9,7 +9,7 @@ function can_do() {
           <div className="bg-black h-0.5 "></div>
         </div>
 
-        <div className="grid 2xl:grid-cols-3 lg:grid-cols-2  relative mt-16  overflow-hidden p-7">
+        <div className="grid 2xl:grid-cols-3 lg:grid-cols-2  relative mt-16  overflow-hidden p-12">
           <div className="flex flex-col items-center">
             <div>
               <a
@@ -51,7 +51,10 @@ function can_do() {
 
           <div className="flex flex-col items-center">
             <div>
-              <a href="https://www.figma.com/proto/7xJNdNuXfmmeVSLFXG7AFu/shop?type=design&node-id=132-14&t=F8pwueNsNTZkTPUq-0&scaling=scale-down&page-id=0%3A1" target="_blank">
+              <a
+                href="https://www.figma.com/proto/7xJNdNuXfmmeVSLFXG7AFu/shop?type=design&node-id=132-14&t=F8pwueNsNTZkTPUq-0&scaling=scale-down&page-id=0%3A1"
+                target="_blank"
+              >
                 {" "}
                 <img
                   className="pb-4 border-b-2 border-black mb-2 hover:scale-105"
@@ -87,37 +90,18 @@ function can_do() {
 
           <div className="flex items-center flex-col">
             <div>
-              <a href="https://www.figma.com/proto/uNQOJ2RWc17GwoI0qzc3b9/veteransponsored?type=design&node-id=1-2&t=f2VIEGgioeJ2TwQr-0&scaling=scale-down&page-id=0%3A1" target="_blank">
+              <a
+                href="https://www.figma.com/proto/uNQOJ2RWc17GwoI0qzc3b9/veteransponsored?type=design&node-id=1-2&t=f2VIEGgioeJ2TwQr-0&scaling=scale-down&page-id=0%3A1"
+                target="_blank"
+              >
                 <img
                   className="pb-4 border-b-2 border-black mb-2 hover:scale-105"
                   src="/pic3.png"
                   alt="myimage"
-                  height={200}
+                  height={300}
                   width={500}
                 />
               </a>
-
-              <div className="grid grid-cols-2">
-                <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
-                  Project
-                </div>
-                <div className="font-tt-commons-pro text-[#544541] text-[14px]">
-                  Veteransponsor
-                </div>
-                <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
-                  Year
-                </div>
-                <div className="font-tt-commons-pro text-[#544541] text-[14px]">
-                  2024
-                </div>
-                <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
-                  Type
-                </div>
-                <div className="font-tt-commons-pro text-[#544541] text-[14px]">
-                  Web design for sponsoring the veteran <br/>
-                <a href="https://veteranshonored.org/services/" target="_blank" className="hover:underline  hover:text-[#58a2f5]"> Original website </a> <br/>
-                </div>
-              </div>
 
               <div className="grid grid-cols-2">
                 <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
@@ -136,8 +120,57 @@ function can_do() {
                   Type
                 </div>
                 <div className="font-tt-commons-pro text-[#544541] text-[14px]">
-                  Web design for mobile app airport in Japan <br/>
-                <a href="https://www.figma.com/proto/RtqZg8YmHdXJrNs6naLowW/management-app-for-airport-in-japan?type=design&node-id=2-2&t=eyiUN3hxaaCD7EuP-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=2%3A2&mode=design" target="_blank" className="hover:underline  hover:text-[#58a2f5]"></a> <br/>
+                  Web design for mobile app airport in Japan <br />
+                  <a
+                    href="https://www.figma.com/proto/RtqZg8YmHdXJrNs6naLowW/management-app-for-airport-in-japan?type=design&node-id=2-2&t=eyiUN3hxaaCD7EuP-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=2%3A2&mode=design"
+                    target="_blank"
+                    className="hover:underline  hover:text-[#58a2f5]"
+                  ></a>{" "}
+                  <br />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex items-center flex-col mt-10">
+            <div>
+              <a
+                href="https://www.figma.com/proto/uNQOJ2RWc17GwoI0qzc3b9/veteransponsored?type=design&node-id=1-2&t=f2VIEGgioeJ2TwQr-0&scaling=scale-down&page-id=0%3A1"
+                target="_blank"
+              >
+                <img
+                  className="pb-4 border-b-2 border-black mb-2 hover:scale-105"
+                  src="/pic4.png"
+                  alt="myimage"
+                  height={300}
+                  width={500}
+                />
+              </a>
+
+              <div className="grid grid-cols-2 ">
+                <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
+                  Project
+                </div>
+                <div className="font-tt-commons-pro text-[#544541] text-[14px]">
+                  Smoothies web design 
+                </div>
+                <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
+                  Year
+                </div>
+                <div className="font-tt-commons-pro text-[#544541] text-[14px]">
+                  2024
+                </div>
+                <div className="font-tt-commons-pro font-bold text-[#544541] text-[14px]">
+                  Type
+                </div>
+                <div className="font-tt-commons-pro text-[#544541] text-[14px]">
+                  Web design for selling smoothies <br />
+                  <a
+                    href="https://www.figma.com/proto/RtqZg8YmHdXJrNs6naLowW/management-app-for-airport-in-japan?type=design&node-id=2-2&t=eyiUN3hxaaCD7EuP-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=2%3A2&mode=design"
+                    target="_blank"
+                    className="hover:underline  hover:text-[#58a2f5]"
+                  ></a>{" "}
+                  <br />
                 </div>
               </div>
             </div>
