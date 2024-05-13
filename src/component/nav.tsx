ae8db6d805da077daecfcc84ec9bd8d64635d08d
zhi_jun <liangzhijun1216@gmail.com>
2024-05-13 10:33:34 -0400

    updated responsive

--- src/component/nav.tsx
+++ src/component/nav.tsx
@@ -1,23 +1,23 @@
 function Nav() {
   return (
-    <> 
+    <>
       <div className="fixed top-0 w-full bg-white bg-opacity-70 shadow-md z-10 transition-all duration-500">
-        <div className="flex justify-center md:px-64  w-full px-0">
-          <div className="flex items-center  py-3 md:gap-[60] gap-5 text-wrap ">
+        <div className="flex justify-center items-center py-3">
+          <div className="flex flex-row text-nowrap overflow-x-auto">
             <a
-              className="font-agrandir font-bold text-[20px] hover:bg-[#735E59] hover:underline hover:text-white rounded-3xl px-14 py-2 transition-all duration-500 transform transition:translate-x-28"
+              className="font-agrandir font-bold text-[20px] hover:bg-[#735E59] hover:underline hover:text-white rounded-3xl px-6 md:px-10 py-2 transition-all duration-500"
               href="#title"
             >
               Bio
             </a>
             <a
-              className="font-agrandir font-bold text-[20px] hover:bg-[#735E59] hover:underline hover:text-white rounded-3xl px-14 py-2 transition-all duration-500 transform transition:translate-x-28"
+              className="font-agrandir font-bold text-[20px] hover:bg-[#735E59] hover:underline hover:text-white rounded-3xl px-6 md:px-10 py-2 transition-all duration-500"
               href="#about_me"
             >
               About me
             </a>
             <a
-              className="font-agrandir font-bold text-[20px] hover:bg-[#735E59] hover:underline hover:text-white rounded-3xl px-14 py-2 transition-all duration-500 transform transition:translate-x-28"
+              className="font-agrandir font-bold text-[20px] hover:bg-[#735E59] hover:underline hover:text-white rounded-3xl px-6 md:px-10 py-2 transition-all duration-500"
               href="#contact_me"
             >
               Contact me
@@ -25,6 +25,7 @@ function Nav() {
           </div>
         </div>
       </div>
+
       <div className="h-16"></div>
     </>
   );
--- src/component/title.tsx
+++ src/component/title.tsx
@@ -5,14 +5,14 @@ import img1 from "../images/myimage.png";
 function Title() {
   return (
     <>
-      <div className="grid md:grid-cols-2 grid-cols-1 gap-10 mt-36 mb-52 w-full px-10"  id="title">
-        <div className="flex flex-col justify-center items-center ">
+      <div className="grid md:grid-cols-2 grid-cols-1 gap-10 md:mt-36 mt-10 mb-52 w-full px-3"  id="title">
+        <div className="flex flex-col md:justify-center md:items-center text-left">
           <div className=" ">
             <p className="font-bold text-[22px] font-tt-commons-pro ">
               Hello everyone! My name is
             </p>
             <Bounce>
-              <h1 className="font-agrandir font-bold md:text-[72px] text-[32px] ">
+              <h1 className="font-agrandir md:font-bold md:text-[72px] text-[52px] text-nowrap ">
                 Zhi Jun Liang
               </h1>
             </Bounce>
